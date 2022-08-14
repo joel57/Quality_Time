4bd262946c4a4d12b57d3bdf200dd8d82ed1d9d5
Dennis Jackson <dennisdjack1987@gmail.com>
2022-08-14 12:33:42 -0700

    add input funtionallity

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,25 @@ let resultEl =  document.querySelector('#date-results')
 
 
 
+let getinputs = document.querySelectorAll('input')
+
+let inputOne  = getinputs[0]
+let inputTwo =  getinputs[1]
 
 
+btnElOne.addEventListener('click',(event)=>{
+    event.preventDefault()
+    foodResultsEL.innerHTML =''
+    getFood(inputOne.value)
+    console.log(inputOne.value)
+})
+
+btnElTwo.addEventListener('click',(event)=>{
+    event.preventDefault()
+    drinkResultsEl.innerHTML = ''
+    getDrink(inputTwo.value)
+    console.log(inputTwo.value)
+})
 
 async function getRandomDrink(){
     let response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
@@ -69,9 +86,27 @@ async function getrandomFood(){
     
 }
 
-async function getDrink(drink){
-    let response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+async function getDrink(d){
+    let response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${d}`)
     let data = await response.json()
+    let drink = data.drinks[0]
+    console.log(drink)
+
+    let drinkContainer =  document.createElement('div')
+    drinkContainer.style.height= '200px'
+    drinkContainer.style.width= '200px'
+    // drinkContainer.style.overflow= 'hidden'
+
+    let drinkName = document.createElement('strong')
+    drinkName.textContent = drink.strDrink
+    let drinkimg = document.createElement('img')
+    drinkimg.style.objectFit = 'contain'
+    drinkimg.setAttribute('src',drink.strDrinkThumb)
+    
+    drinkContainer.appendChild(drinkName)
+    drinkContainer.appendChild(drinkimg)
+
+    drinkResultsEl.appendChild(drinkContainer)
     // let drink = data.foods[0]
 
     
@@ -81,10 +116,35 @@ async function getDrink(drink){
     
 }
 
-async function getFood(food){
-    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
+async function getFood(f){
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${f}`)
     let data = await response.json()
-    return data
+    let food  = data.meals[0]
+    let regex = /(?<=\=).+/
+
+
+    let foodContainer = document.createElement('div')
+    foodContainer.style.height = '200px'
+    foodContainer.style.widows = '200px'
+    
+    let foodname = document.createElement('strong')
+    foodname.textContent = food.strMeal
+    
+    let foodvid = document.createElement('iframe')
+    foodvid.setAttribute('src',`https://www.youtube.com/embed/${food.strYoutube.match(regex)[0]}`)
+    // foodvid.setAttribute('width','560')
+    // foodvid.setAttribute('height','315')
+    foodvid.setAttribute('title','YouTube video player')
+    foodvid.setAttribute('frameborder','0')
+    // foodvid.setAttribute('encrypted-media')
+    // foodvid.setAttribute()
+
+    foodContainer.appendChild(foodname)
+    foodContainer.appendChild(foodvid)
+    foodResultsEL.appendChild(foodContainer)
+    // let regex = /(?<=\=).+/
+
+    console.log(food.strYoutube.match(regex)[0])
     
 }
 
