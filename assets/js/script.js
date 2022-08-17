cb690e4cedff1a810114657c127082e44c792d85
kimshady93 <kimberly.m.oaks@gmail.com>
2022-08-16 20:03:21 -0700

    add basic navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -318,4 +318,10 @@ async function getFood(f){
 }
 
 
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
 
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
